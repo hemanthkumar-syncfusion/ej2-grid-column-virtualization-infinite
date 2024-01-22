ff27540d278bff4fd813cefb58773c55228d811c
hemanthkumar-syncfusion <96279305+hemanthkumar-syncfusion@users.noreply.github.com>
2024-01-22 12:29:39 +0530

    Create data.js

---
+++ data.js
@@ -0,0 +1,52 @@
+var virtualData = [];
+    function datasource() {
+        var names = ['VINET', 'TOMSP', 'HANAR', 'VICTE', 'SUPRD', 'HANAR', 'CHOPS', 'RICSU', 'WELLI', 'HILAA', 'ERNSH', 'CENTC',
+            'OTTIK', 'QUEDE', 'RATTC', 'ERNSH', 'FOLKO', 'BLONP', 'WARTH', 'FRANK', 'GROSR', 'WHITC', 'WARTH', 'SPLIR', 'RATTC', 'QUICK', 'VINET',
+            'MAGAA', 'TORTU', 'MORGK', 'BERGS', 'LEHMS', 'BERGS', 'ROMEY', 'ROMEY', 'LILAS', 'LEHMS', 'QUICK', 'QUICK', 'RICAR', 'REGGC', 'BSBEV',
+            'COMMI', 'QUEDE', 'TRADH', 'TORTU', 'RATTC', 'VINET', 'LILAS', 'BLONP', 'HUNGO', 'RICAR', 'MAGAA', 'WANDK', 'SUPRD', 'GODOS', 'TORTU',
+            'OLDWO', 'ROMEY', 'LONEP', 'ANATR', 'HUNGO', 'THEBI', 'DUMON', 'WANDK', 'QUICK', 'RATTC', 'ISLAT', 'RATTC', 'LONEP', 'ISLAT', 'TORTU',
+            'WARTH', 'ISLAT', 'PERIC', 'KOENE', 'SAVEA', 'KOENE', 'BOLID', 'FOLKO', 'FURIB', 'SPLIR', 'LILAS', 'BONAP', 'MEREP', 'WARTH', 'VICTE',
+            'HUNGO', 'PRINI', 'FRANK', 'OLDWO', 'MEREP', 'BONAP', 'SIMOB', 'FRANK', 'LEHMS', 'WHITC', 'QUICK', 'RATTC', 'FAMIA'];
+        var sport = ['Cricket', 'Football', 'Tennis', 'Golf', 'Chess', 'Dodgeball', 'Racket', 'Archery', 'Climbing', 'Hunting', 'Carrom', 'Tag', 'Novuss',
+            'Subbuteo', 'Baseball', 'Madden NFL', 'Shuffleboard', 'Badminton', 'Hockey', 'Volleyball', 'Table Tennis', 'Golf', 'Cycling', 'Running', 'Walking', 'Wireball',
+            'Town ball', 'Tee ball', 'Stool ball', 'Stick ball'];
+        var country = ['India', 'Australia', 'Ballesteros', 'Belgium', 'Brazil', 'England', 'Ethiopia', 'Finland', 'France', 'Germany', 'Britain',
+            'Argentina', 'Jamaica', 'Kenya', 'Morocco', 'Ireland', 'Norway', 'Philippines', 'Portugal', 'Romania', 'Russia', 'Scotland', 'Scottish', 'Serbia', 'Spain',
+            'Sweden', 'Switzerland', 'Netherlands', 'UK', 'Ukraine', 'US', 'Wales', 'West Indies', 'China', 'Hong Kong', 'Italy', 'Philippines', 'Turkey', 'Botswana',
+            'Sri Lanka', 'Algeria', 'Bangladesh', 'Egypt', 'Malaysia'];
+        for (var i = 0; i < 301; i++) {
+            exports.virtualData.push({
+                'SNo': (i + 1),
+                'FIELD1': (i + 1) + ' FIELD1 ' + names[Math.floor(Math.random() * names.length)],
+                'FIELD2': (i + 1) + ' FIELD2 ' + 1967 + (i % 10),
+                'FIELD3': (i + 1) + ' FIELD3 ' + sport[Math.floor(Math.random() * sport.length)],
+                'FIELD4': (i + 1) + ' FIELD4 ' + country[Math.floor(Math.random() * country.length)],
+                'FIELD5': (i + 1) + ' FIELD5 ' + Math.floor(Math.random() * 2000),
+                'FIELD6': (i + 1) + ' FIELD6 ' + Math.floor(Math.random() * 1000),
+                'FIELD7': (i + 1) + ' FIELD7 ' + Math.floor(Math.random() * 100),
+                'FIELD8': (i + 1) + ' FIELD8 ' + Math.floor(Math.random() * 10),
+                'FIELD9': (i + 1) + ' FIELD9 ' + Math.floor(Math.random() * 10),
+                'FIELD10': (i + 1) + ' FIELD10 ' + Math.floor(Math.random() * 100),
+                'FIELD11': (i + 1) + ' FIELD11 ' + Math.floor(Math.random() * 100),
+                'FIELD12': (i + 1) + ' FIELD12 ' + Math.floor(Math.random() * 1000),
+                'FIELD13': (i + 1) + ' FIELD13 ' + Math.floor(Math.random() * 10),
+                'FIELD14': (i + 1) + ' FIELD14 ' + Math.floor(Math.random() * 10),
+                'FIELD15': (i + 1) + ' FIELD15 ' + Math.floor(Math.random() * 1000),
+                'FIELD16': (i + 1) + ' FIELD16 ' + Math.floor(Math.random() * 200),
+                'FIELD17': (i + 1) + ' FIELD17 ' + Math.floor(Math.random() * 300),
+                'FIELD18': (i + 1) + ' FIELD18 ' + Math.floor(Math.random() * 400),
+                'FIELD19': (i + 1) + ' FIELD19 ' + Math.floor(Math.random() * 500),
+                'FIELD20': (i + 1) + ' FIELD20 ' + Math.floor(Math.random() * 700),
+                'FIELD21': (i + 1) + ' FIELD21 ' + Math.floor(Math.random() * 800),
+                'FIELD22': (i + 1) + ' FIELD22 ' + Math.floor(Math.random() * 1000),
+                'FIELD23': (i + 1) + ' FIELD23 ' + Math.floor(Math.random() * 2000),
+                'FIELD24': (i + 1) + ' FIELD24 ' + Math.floor(Math.random() * 150),
+                'FIELD25': (i + 1) + ' FIELD25 ' + Math.floor(Math.random() * 1000),
+                'FIELD26': (i + 1) + ' FIELD26 ' + Math.floor(Math.random() * 100),
+                'FIELD27': (i + 1) + ' FIELD27 ' + Math.floor(Math.random() * 400),
+                'FIELD28': (i + 1) + ' FIELD28 ' + Math.floor(Math.random() * 600),
+                'FIELD29': (i + 1) + ' FIELD29 ' + Math.floor(Math.random() * 500),
+                'FIELD30': (i + 1) + ' FIELD30 ' + Math.floor(Math.random() * 300),
+            });
+        }
+    }
